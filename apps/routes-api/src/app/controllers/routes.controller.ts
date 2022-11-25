adcc2d710c06be92fe86f2ff2b5e4c478b7fdc59
ssuarez16 <sergiiosuarez16@gmail.com>
2022-11-25 23:35:31 +0100

    posting Update method for routes-api

--- apps/routes-api/src/app/controllers/routes.controller.ts
+++ apps/routes-api/src/app/controllers/routes.controller.ts
@@ -12,17 +12,12 @@ class RoutesController {
 
   public updateRoute = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const idRoute = req.params.id
-      const route = await this.routesService.getRouteById(idRoute)
+
       const newRoute = req.body
-      
-        if (route !=  null){
-            this.routesService.updateRoute(newRoute,idRoute)
-            res.sendStatus(201)
-        }else{
-            this.routesService.createRoute(newRoute)
-            res.sendStatus(201)
-        }
+      const idRoute = req.params.id
+      this.routesService.updateRoute(newRoute,idRoute)
+      res.sendStatus(201)
+
 
     } catch (error) {
       next(error)
--- apps/routes-api/src/app/services/routes.service.ts
+++ apps/routes-api/src/app/services/routes.service.ts
@@ -2,19 +2,8 @@ import {Route, RouteModel} from "../models/route.model"
 
 export class RoutesService{
 
-    async getRouteById(id: string): Promise<Route> {
-        return RouteModel
-          .findById(id)
-          .exec()
-    }
-    async createRoute(newRoute: Route): Promise <Route> {
-        const result = await RouteModel
-         .create(newRoute)
-        return result
-    }
     async updateRoute(putRoute: Route,id: string): Promise <Route> {
-            const result = await RouteModel
-             .findByIdAndUpdate(id,putRoute)
+            const result = await RouteModel.findByIdAndUpdate(id,putRoute)
             return result
         
     } 
