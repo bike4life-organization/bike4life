b870030938360e9436479466b84fb8b65a9d3791
josdelser <joseds_6@outlook.com>
2022-11-27 16:20:08 +0100

    nunjuncks integrated without template

--- apps/notifier/src/pull-listener.ts
+++ apps/notifier/src/pull-listener.ts
@@ -2,6 +2,9 @@ import { logger, PubSubClient } from '@bike4life/commons'
 import { Message } from '@google-cloud/pubsub'
 import  sgMail = require('@sendgrid/mail')
 import fs = require("fs");
+import nunjucks = require("nunjucks");
+import path = require('path');
+import { toUSVString } from 'util';
 
 
 const { PUBSUB_PROJECT_ID, NOTIFIER_PUBSUB_SUBSCRIPTION_NAME, NOTIFIER_PUBSUB_TOPIC_NAME, TO_REPLY_TO_EMAIL_SENDGRID, SENDGRID_APY_KEY } = process.env
@@ -27,27 +30,63 @@ export default async function startPullListener(): Promise<void> {
 
       const pathToAttachment = `${__dirname}/src/assets/views/hola.html`;
       //const attachment = fs.readFileSync(pathToAttachment,'utf8');
+      console.log("antes del template")
+      nunjucks.configure('views',{autoescape: true});
+      console.log("en medio del template") 
+      // D:\Escritorio\Master\Fundamentos de cloud\bike4life\apps\notifier\src\views\base.html
+      // D:\Escritorio\Master\Fundamentos de cloud\bike4life\apps\notifier\src\pull-listener.ts
+
+      const Nuntemplate = nunjucks.render('/views\/base.html',dataJson);
+      console.log("despues del template")
     
-      const msg = {
-        to: dataJson.user_email,
-        from: TO_REPLY_TO_EMAIL_SENDGRID,
-        reply_to: TO_REPLY_TO_EMAIL_SENDGRID,
-        subject: 'Sending with SendGrid is Fun VAMOSSSSS BRRRRR',
-        text: "hola",
-        /*attachments: [
-          {
-            content: attachment,
-            filename: "attachment.html",
-            type: "text/html",
-            disposition: "attachment"
-          }
-        ]*/
-      }
+      
 
       if(topic === "UserCorrectlyCreated") {
         console.log('hola');
+        const msg = {
+          to: dataJson.user_email,
+          from: TO_REPLY_TO_EMAIL_SENDGRID,
+          reply_to: TO_REPLY_TO_EMAIL_SENDGRID,
+          subject: 'User created correctly',
+          content:[
+            {
+              type: 'text/html',
+              value: Nuntemplate
+            }
+          ],
+          text: "hola",
+          /*attachments: [
+            {
+              content: attachment,
+              filename: "attachment.html",
+              type: "text/html",
+              disposition: "attachment"
+            }
+          ]*/
+        }
       } else if(topic === "RouteOptimizedNotification"){
         console.log('hoal2');
+        const msg = {
+          to: dataJson.user_email,
+          from: TO_REPLY_TO_EMAIL_SENDGRID,
+          reply_to: TO_REPLY_TO_EMAIL_SENDGRID,
+          subject: 'Route Optimized',
+          content:[
+            {
+              type: 'text/html',
+              value: Nuntemplate
+            }
+          ],
+          text: "hola",
+          /*attachments: [
+            {
+              content: attachment,
+              filename: "attachment.html",
+              type: "text/html",
+              disposition: "attachment"
+            }
+          ]*/
+        }
       }
 
       sgMail.send(msg).catch(err => {
