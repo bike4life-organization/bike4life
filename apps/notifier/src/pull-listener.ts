fa120e51ca351269d0ca09e99770e355bfd413bd
josdelser <joseds_6@outlook.com>
2022-11-28 19:46:16 +0100

    Nunjucks templates working correctly

--- apps/notifier/src/pull-listener.ts
+++ apps/notifier/src/pull-listener.ts
@@ -7,9 +7,6 @@ import * as nunjucks from "nunjucks";
 
 import { sendgridSettings as sd } from './app/settings';
 
-//import path = require('path');
-//import { toUSVString } from 'util';
-
 const pubsubSettings = {
   projectId: sd.PUBSUB_PROJECT_ID,
   subscriptionName: sd.NOTIFIER_PUBSUB_SUBSCRIPTION_NAME,
@@ -19,8 +16,7 @@ const pubsubSettings = {
 export default async function startPullListener(): Promise<void> {
   const pubsubClient = new PubSubClient(pubsubSettings.projectId, logger)
   const sgMail = new MailService();
-
-  const myFile = __dirname + '/assets/views/hola.html';
+  const myFile = __dirname + '/assets/views';
 
   try {
     const subscription = await pubsubClient.getSubscription(pubsubSettings.topicName, pubsubSettings.subscriptionName)
@@ -33,39 +29,28 @@ export default async function startPullListener(): Promise<void> {
       const { topic } = attributes;
       const dataJson = JSON.parse(message.data.toString());
 
-      //read html
-      const readData = fs.readFileSync(myFile, 'utf8');
-      console.log(readData)
-      //const pathToAttachment = `${__dirname}/src/assets/views/base.html`;
-      //const attachment = fs.readFileSync(pathToAttachment,'utf8');
-      
-      //nunjucks.configure(__dirname+'/src/views/',{autoescape: true});
-      //const nunTemplate = nunjucks.render('base.html',dataJson);
-      //console.log(__dirname+'/src/views/');
+      nunjucks.configure(myFile,{autoescape: true});
+      const templateUserCreated = nunjucks.render("usercreated.html",dataJson);
+      const templateRouteOptimized = nunjucks.render("routeoptimized.html",dataJson);
+      //Get the name of the user to put in the templates
 
-      console.log(__dirname)
     
       const msg = {
         to: dataJson.user_email,
         from: sd.TO_REPLY_TO_EMAIL_SENDGRID,
         reply_to: sd.TO_REPLY_TO_EMAIL_SENDGRID,
-        subject: 'User created correctly',
-        text: "hola",
-        html: readData,
-        /*attachments: [
-          {
-            content: attachment,
-            filename: "attachment.html",
-            type: "text/html",
-            disposition: "attachment"
-          }
-        ]*/
+        subject: "",
+        html: null
       }
 
       if(topic === "UserCorrectlyCreated") {
-        console.log('hola');
+        msg.subject = "User created correctly" 
+        msg.html = templateUserCreated
+         
+ 
       } else if(topic === "RouteOptimizedNotification"){
-        console.log('hoal2');
+        msg.subject = "Route optimized correctly" 
+        msg.html = templateRouteOptimized
       }
 
       sgMail.send(msg).catch(err => {
