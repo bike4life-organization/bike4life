2f30de32ef22b845f9d3bd84ca98996267a88f6c
josdelser <joseds_6@outlook.com>
2022-12-08 13:27:04 +0100

    handler route-optimized done

--- apps/notifier/src/app/handlers/route-optimized.ts
+++ apps/notifier/src/app/handlers/route-optimized.ts
@@ -1,3 +1,19 @@
-export async function routeOptimizedHandler() {
+import { RouteOptimizedEventMessage } from "@bike4life/commons";
+import nunjucksService from "../services/nunjucks.service";
+import sendGridService from "../services/sendgrid.service";
 
-}
+    function isRouteOptimizedEventMessage(message: unknown): message is RouteOptimizedEventMessage {
+        return typeof message === 'object' && message !== null && 'route_id' in message && 'user_id' in message && 'user_email' in message;
+      }
+      
+      export async function routeOptimizedHandler(payload: unknown) {
+        if (!isRouteOptimizedEventMessage(payload)) {
+          throw new Error('Invalid payload');
+        }
+        const subject = 'Your route';
+        const emailTemplate = 'routeOptimizedCorrectly.html'
+        const template = nunjucksService.obtainTemplate(emailTemplate, payload);
+      
+        await sendGridService.sendEmailWithTemplate(subject, payload.user_email, template);
+      }
+      
--- libs/commons/src/lib/pubsub/notifier-messages.ts
+++ libs/commons/src/lib/pubsub/notifier-messages.ts
@@ -2,3 +2,9 @@ export interface UserCreatedEventMessage {
   user_id: string;
   user_email: string;
 }
+
+export interface RouteOptimizedEventMessage {
+  route_id: string;
+  user_id: string;
+  user_email: string;
+}
