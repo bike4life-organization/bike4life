99c30c9693cbda875fb90308dd8c0e93f6153762
Moisés Calzado <mcalzado@cartodb.com>
2022-12-14 00:30:24 +0100

    Add config to show swagger info

--- apps/routes-api/src/app/app.ts
+++ apps/routes-api/src/app/app.ts
@@ -63,7 +63,7 @@ class App {
       definition: {
         openapi: '3.0.0',
         info: {
-          title: 'Hello World',
+          title: 'Routes API',
           version: '1.0.0',
         },
       },
