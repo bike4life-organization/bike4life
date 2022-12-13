0597adab6b8ed5791b15bbe4247cb88c1112f7b5
josdelser <joseds_6@outlook.com>
2022-12-13 23:05:03 +0100

    swagger config

--- apps/notifier/src/app/app.ts
+++ apps/notifier/src/app/app.ts
@@ -1,12 +1,20 @@
 import mongoose, { set, connect } from "mongoose";
 import startPullListener from "./listener/pull-listener";
+import * as express from "express";
+import * as swaggerJSDoc from "swagger-jsdoc";
+import * as swaggerUi from "swagger-ui-express";
+import * as path from "path";
 import { mongoConnectionSettings } from "./settings";
+import { Application } from "express";
 
 class App {
-  env: string
-  
+  app: Application;
+  env: string;
+
   constructor() {
+    this.app = express();
     this.env = process.env.NODE_ENV || "development";
+    this.initializeSwagger();
   }
 
   listen(): Promise<void> {
@@ -26,6 +34,23 @@ class App {
 
   async stop(): Promise<void> {
     await mongoose.connection.close();
+  }
+
+  private initializeSwagger(): void {
+    const options = {
+      failOnErrors: true,
+      definition: {
+        openapi: "3.0.0",
+        info: {
+          title: "Hello World",
+          version: "1.0.0",
+        },
+      },
+      apis: [path.join(__dirname, "swagger.yaml")],
+    };
+
+    const specs = swaggerJSDoc(options);
+    this.app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
   }
 }
 
