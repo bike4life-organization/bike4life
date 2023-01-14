8484e62743dbe9221bbac3502d53cd6729b08c13
josdelser <joseds_6@outlook.com>
2023-01-14 18:21:15 +0100

    test

---
+++ apps/notifier/src/__tests__/handler/route.created.test.ts
@@ -0,0 +1,3 @@
+import { routeCreatedHandler } from "../../app/handlers/route-created";
+
+jest.mock("../../app/handlers/route-created");
---
+++ apps/notifier/src/__tests__/service/sendgrid.test.ts
@@ -0,0 +1,29 @@
+import sendGridService = require("../../app/services/sendgrid.service");
+import { MailService } from "@sendgrid/mail";
+jest.mock("@sendgrid/mail");
+
+describe("SendGrid Service", () => {
+  it("should send email with template", async () => {
+    // mock the payload
+    const payload = {
+      user_email: "test@example.com",
+      user_id: "123",
+      route_id: "456",
+    };
+
+    // mock the template
+    const template = "<p>Hello,</p> <p>Test email</p>";
+
+    // mock the event type
+    const type = "test";
+
+    // mock the send method
+    MailService.send.mockResolvedValue(null);
+
+    // call the sendEmailWithTemplate method
+    await sendGridService.send("Test Subject", payload, template, type);
+
+    // assert that the send method was called
+    expect(MailService.send).toHaveBeenCalled();
+  });
+});
