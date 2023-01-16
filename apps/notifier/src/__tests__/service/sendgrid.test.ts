b2eab067ebde92fcfa4be8c5f73d1218c1dead38
Moisés Calzado <mcalzado@cartodb.com>
2023-01-16 17:12:51 +0100

    Fix tests

--- apps/notifier/src/__tests__/service/sendgrid.test.ts
+++ apps/notifier/src/__tests__/service/sendgrid.test.ts
@@ -1,9 +1,20 @@
-import sendGridService = require("../../app/services/sendgrid.service");
-import { MailService } from "@sendgrid/mail";
 jest.mock("@sendgrid/mail");
+import { MailService } from "@sendgrid/mail";
+
+// mock the send method
+MailService.prototype.send = jest.fn()
+MailService.prototype.setApiKey = jest.fn()
+
+import sendGridService from "../../app/services/sendgrid.service";
+import { NotifierMessageTypes } from '@bike4life/commons'
+import { NotificationModel } from '../../app/models/notification.model'
+import { mockNotification } from '../support/notification'
 
 describe("SendGrid Service", () => {
   it("should send email with template", async () => {
+    jest.spyOn(NotificationModel, 'create').mockImplementationOnce(async () => {
+      return mockNotification
+    });
     // mock the payload
     const payload = {
       user_email: "test@example.com",
@@ -15,10 +26,7 @@ describe("SendGrid Service", () => {
     const template = "<p>Hello,</p> <p>Test email</p>";
 
     // mock the event type
-    const type = "test";
-
-    // mock the send method
-    MailService.send.mockResolvedValue(null);
+    const type = NotifierMessageTypes.ROUTE_CREATED
 
     // call the sendEmailWithTemplate method
     await sendGridService.sendEmailWithTemplate(
@@ -29,6 +37,7 @@ describe("SendGrid Service", () => {
     );
 
     // assert that the send method was called
-    expect(MailService.send).toHaveBeenCalled();
+    expect(MailService.prototype.setApiKey).toHaveBeenCalled()
+    expect(MailService.prototype.send).toHaveBeenCalled();
   });
 });
